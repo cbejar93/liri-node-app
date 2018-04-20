6e13e9a836ea3201934b3549e073399f9a293b13
unknown <cbejar93@gmail.com>
2018-04-20 19:42:41 -0400

    got twiiter working

--- liri.js
+++ liri.js
@@ -1,28 +1,57 @@
 require("dotenv").config();
 
+var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
 var request = require("request");
+console.log(keys.twitter);
+
+
+
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-var userInput = process.argv[2];
-var nodeArg= "";
-for (var i =3; process.argv.length; i++){
-    nodeArg += process.argv[i] + "+" ;
+// var userInput = process.argv[2];
+// var nodeArg= "";
+// for (var i = 3 ; process.argv.length; i++){
+//     nodeArg += process.argv[i] + "+" ;
+// }
+
+// switch (userInput){
+//     case `my-tweets`:
+//         myTweets();
+//         break;
+//     case `spotify-this-song`:
+//         spotify();
+//         break;
+//     case `movie-this`:
+//         break;
+//     case `do-what-it-says`:
+//     break;
+// }
+
+function myTweets(){
+  
+    client.get("statuses/user_timeline", function(error, tweets, response) {
+
+        console.log(tweets); 
+      })
 }
 
-switch (userInput){
-    case `my-tweets`:
-        // mytweetfunction 
-        break;
-    case `spotify-this-song`:
-        // myspotifyfunction
-        break;
-    case `movie-this`:
-        break;
-    case `do-what-it-says`:
-    break;
-}
+myTweets();
+
+// function spotify(){
+//     spotify.search({ type: 'track', query: nodeArg }, function(err, data) {
+//         if (err) {
+//           return console.log('Error occurred: ' + err);
+//         }
+       
+//       console.log(data); 
+//       });
+// }
+
+
+
+
 
